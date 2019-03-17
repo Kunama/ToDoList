8b05d79fee402b442dbca0c375896fbd77cab92f
Akhil Kunam <kunamakhil@gmail.com>
2019-03-17 15:02:24 -0600

    Added serviceWorker.js back to fix issue with Now

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import ToDoList from "./ToDoList";
-
-ReactDOM.render(<ToDoList />, document.getElementById("root"));
+import * as serviceWorker from './serviceWorker';
+ReactDOM.render( < ToDoList / > , document.getElementById("root"));
+serviceWorker.unregister();
