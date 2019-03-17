a3d43fb140c21f7f37f9bdf439db28b004eaf85d
Akhil Kunam <kunamakhil@gmail.com>
2019-03-17 15:29:02 -0600

    Deleted homepage from package to fix Now deploy

--- src/index.js
+++ src/index.js
@@ -1,6 +1,4 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import ToDoList from "./ToDoList";
-import * as serviceWorker from './serviceWorker';
-ReactDOM.render( < ToDoList / > , document.getElementById("root"));
-serviceWorker.unregister();
+ReactDOM.render( <ToDoList/> , document.getElementById("root"));
