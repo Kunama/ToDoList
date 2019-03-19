5b487fe5166259d1cf4dc0cb7c6cd691e5d988bd
Akhil Kunam <kunamakhil@gmail.com>
2019-03-18 20:43:40 -0600

    Added some styling and completed task feature

--- src/ToDoList.js
+++ src/ToDoList.js
@@ -1,24 +1,33 @@
 import React, { Component } from "react";
-
+import "./main.css";
 class TaskDisplay extends Component {
   constructor(props){
     super(props);
     this.displayTasks = this.displayTasks.bind(this);
     this.deleteTask = this.deleteTask.bind(this);
   }
-
+  toggleCompletion(e, key){
+    e.preventDefault();
+    this.props.completion(key);
+  }
   deleteTask(e, key){
     e.preventDefault();
     this.props.delete(key);
   }
 
   displayTasks(task) {
-    return <div key ={task.key}><li key={task.key}>{task.value}</li><button onClick = {(e) => this.deleteTask(e, task.key)}>Delete</button></div>;
+    
+    if(task.completed === true){
+      var style={ color: '#8CC739' }
+    } else{
+      style={ color: '' }
+    }
+    return <div key ={task.key}><li style = {style} key={task.key} onClick={(e) => this.toggleCompletion(e, task.key)}>{task.value}</li><button onClick = {(e) => this.deleteTask(e, task.key)}>Delete</button></div>;
   }
 
   render() {
     var taskList = this.props.tasks.map(this.displayTasks);
-    return <ul>{taskList}</ul>;
+    return <ol>{taskList}</ol>;
   }
 }
 
@@ -27,7 +36,7 @@ class ToDoList extends Component {
     super(props);
     this.createTask = this.createTask.bind(this);
     this.deleteTask = this.deleteTask.bind(this);
-
+    this.toggleCompletion = this.toggleCompletion.bind(this);
     this.state = {
       tasks: [],
       taskCounter: 0
@@ -40,7 +49,8 @@ class ToDoList extends Component {
 
     var task = {
       value: document.getElementById("taskInput").value,
-      key: this.state.taskCounter
+      key: this.state.taskCounter,
+      completed: false
     };
 
     document.getElementById("taskInput").value = ""; //Clear form input
@@ -57,10 +67,21 @@ class ToDoList extends Component {
         taskCounter: updatedTaskCounter
       });
 
-      console.log(this.state.tasks);
+      // console.log(this.state.tasks);
     }
   }
-
+  toggleCompletion(key){
+    
+    var updatedTaskList = this.state.tasks;
+    for(var i=0; i<updatedTaskList.length; i++){
+      if(updatedTaskList[i].key === key){
+        updatedTaskList[i].completed = !updatedTaskList[i].completed;
+      }
+    }
+    this.setState({
+      tasks: updatedTaskList
+    });
+  }
   deleteTask(key){
     var updatedTaskList = this.state.tasks;
     for(var i=0; i<updatedTaskList.length; i++){
@@ -69,6 +90,7 @@ class ToDoList extends Component {
       }
     } 
 
+    
     this.setState({
       tasks: updatedTaskList
     });
@@ -76,15 +98,15 @@ class ToDoList extends Component {
   render() {
     return (
       <div>
-        <form>
+        <form id="taskForm">
           <label>
-            Task: <input type="text" name="task" id="taskInput" />
-          </label>
+            Task: </label><input type="text" name="task" id="taskInput" />
+          
           <button type="submit" onClick={this.createTask}>
             Add Task
           </button>
-          <TaskDisplay tasks={this.state.tasks} delete = {this.deleteTask}/>
         </form>
+        <TaskDisplay tasks={this.state.tasks} delete = {this.deleteTask} completion = {this.toggleCompletion}/>
       </div>
     );
   }
